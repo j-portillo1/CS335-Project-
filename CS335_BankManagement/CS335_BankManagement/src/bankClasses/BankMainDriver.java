e912a7f7387d5d771b22145d4b4f5f13f3bad145
j-portillo1 <144365700+j-portillo1@users.noreply.github.com>
2024-02-20 01:10:45 -0500

    Add files via upload
    Second Commit

---
+++ CS335_BankManagement/CS335_BankManagement/src/bankClasses/BankMainDriver.java
@@ -0,0 +1,42 @@
+package bankClasses;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class BankMainDriver{
+	public static void main(String [] args) throws Exception{
+		CustomerFactory cF = new CustomerFactory("data/CustomerList.csv");
+		ArrayList<Customer> cusList = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		System.out.printf("Please enter L to logIn or enter C if you are a new customer:  ");
+	    String choice = scanner.nextLine();
+	    if (choice.equals("L")){
+	    	logIn userLogIn = new logIn();
+			for(Customer item: cusList) {
+				userLogIn.addUser(item.getCustomerID(), item.getPassword());
+			}
+
+			userLogIn.authenticateUser();
+
+
+
+	    }if (choice.equals("C")) {
+
+			while (cF.hasMoreData()) {
+				cusList.add(cF.getNextCustomer());
+			}
+
+
+			CustomerFactory cFCustomer = new CustomerFactory();
+			Customer cus1 = cFCustomer.makeCustomer();
+			System.out.println(cus1);
+
+
+	    }else {
+	    	System.out.println("Invalid entry. Please try again.");
+	    }
+
+	}
+
+
+}
---
+++ CS335_BankManagement/CS335_BankManagement/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ */
+/**
+ *
+ */
+module CS335_BankManagement {
+}
