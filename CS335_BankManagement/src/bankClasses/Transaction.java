c206c1c6adeb1567a3cebadd3aa28214fd869dbe
MaameAndoh <115769701+MaameAndoh@users.noreply.github.com>
2024-03-27 12:16:28 -0400

    Add Transaction class

---
+++ CS335_BankManagement/src/bankClasses/Transaction.java
@@ -0,0 +1,38 @@
+package bankClasses;
+
+public class Transaction {
+	protected String storeName;
+	protected int transAmount;
+	protected int accID;
+	
+	Transaction(String name, int num, int id){
+		this.storeName = name;
+		this.transAmount = num;
+		this.accID = id;
+	}
+	
+	public int getAccNum() {
+		return accID;
+	}
+		
+	public String getStName() {
+		return storeName;
+	}
+	
+	public int getTransAmount() {
+		return transAmount;
+	}
+	
+//	public int updateBalance() {
+//		boolean dep = true;
+//		if (dep) {
+//			account balance =- transAmount
+//		} else {
+//			account balance =+ transAmount
+//		}
+//	}
+	
+	public String toString() {
+		return("Store: " + storeName + ", Amount: " + transAmount + ", Ending balance: ");
+	}
+}
