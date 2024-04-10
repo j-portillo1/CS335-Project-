54ae1750e935d5994fa518d7fe0b26b200d299dc
Owner <Owner@10.140.124.216>
2024-04-10 12:44:17 -0400

    Attempt at ATM

--- CS335_BankManagement/src/bankClasses/BankMainDriver.java
+++ CS335_BankManagement/src/bankClasses/BankMainDriver.java
@@ -35,8 +35,24 @@ class BankMainDriver{
 			System.out.println(cus1);
 	    	
 	    }
-			
+	    
+	  //test card validation for ATM - First attempt
+
+	    DiscoverCard disCard = new DiscoverCard("6011000990139424", 200, 500, 199, cusList.get(0));
+	    String validateDisc = disCard.validateCardType("6011000990139424");
+
+	    VisaCard visCard = new VisaCard("4012888888881881", 650, 1000, 321, cusList.get(3));
+	    String validateVis = visCard.validateCardType("4012888888881881");
+
+	    MasterCard masCard = new MasterCard("5413330089010640", 90, 200, 002, cusList.get(7));
+	    String validateMas = masCard.validateCardType("5413330089010640");
+
+	    if (!validateDisc.equals("Invalid") || !validateVis.equals("Invalid") || !validateMas.equals("Invalid")) {
+	    	System.out.println("You have entered your account on the ATM");
+	    } else {
+	    	System.out.println("The card is invalid");
+	    }
+	
 	}
 		
-
 }
